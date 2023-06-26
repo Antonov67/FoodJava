63ce50f752886d8c1026e33034626deba090bb4c
Dm Ant <Dm@Ant.ru>
2023-06-26 08:44:23 +0300

    new token

--- app/src/main/java/com/example/foodjava/controller/Utils.java
+++ app/src/main/java/com/example/foodjava/controller/Utils.java
@@ -10,7 +10,7 @@ public class Utils {
 
     final public static String folderId = "b1gs3no6km88s8n7olhu";
     //токен "живет" 12 часов
-    final public static String token ="Bearer " + "t1.9euelZrPz4yRk5DKzMrOm5bHjZCcje3rnpWam5fIzpCbyZqNmZyanpidlpXl8_d-CCtb-e9mZlc8_d3z9z43KFv572ZmVzz9zef1656VmsyKjZWJl5iUypyeypOZm8eK7_zF656VmsyKjZWJl5iUypyeypOZm8eK.qJ5a8thQh-egf8KQdeBw83kEPrMaXvl7OZ6cCw0KvnFT84t7bBMPwOBxgFDzEpi3ud55EbPMe9TXyNISloe2Dw";
+    final public static String token ="Bearer " + "t1.9euelZqTmM6Zx5TLkMjHyZGSlZSZx-3rnpWam5fIzpCbyZqNmZyanpidlpXl9PdlNxtb-e86AhbL3fT3JWYYW_nvOgIWy83n9euelZrLnpqZy8eOyZWdi8jNlMvLi-_8xeuelZrLnpqZy8eOyZWdi8jNlMvLiw.7lwjcXrlVOcbQWO15PgjHe1j_jfvyjCRRNPgXdN4R822VMAD4l5-wXAF_RV0oO-rhmTYqlYGbL1eZSGR6qiSAQ";
     //язык, на который нужно перевести
     final public static String language = "en";
 }
